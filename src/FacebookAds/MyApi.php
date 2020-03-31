3a8592ae2e2487baa9efe0e691e41bec768c7cf1
Serega <serega@actionads.ru>
2020-03-31 20:26:39 +0300

    fix

--- src/FacebookAds/MyApi.php
+++ src/FacebookAds/MyApi.php
@@ -5,18 +5,21 @@ namespace FacebookAds;
 use FacebookAds\Http\Client;
 use FacebookAds\Http\Headers;
 use FacebookAds\Http\Adapter\CurlAdapter;
+use ArrayObject;
+
 class MyApi extends Api
 {
     /**
      * @param string $access_token
      * @param string $socks
+     * @param string $version
      * @return static
      */
-    public static function getApi($access_token, $socks) {
+    public static function getApi($access_token, $socks, $version = Api::VERSION) {
         $session = new MySession(1, 1, $access_token);
         $client = new Client();
         $client->setDefaultRequestHeaders(new Headers(array(
-            'User-Agent' => 'fb-php-'.Api::VERSION,
+            'User-Agent' => 'fb-php-'.$version,
             'Accept-Encoding' => '*',
         )));
         $curlAdapter = new CurlAdapter($client);
@@ -31,7 +34,7 @@ class MyApi extends Api
             $opts[CURLOPT_PROXY] = $socks;
             $opts[CURLOPT_PROXYTYPE] = CURLPROXY_SOCKS5_HOSTNAME;
         }
-        $curlAdapter->setOpts(new \ArrayObject($opts));
+        $curlAdapter->setOpts(new ArrayObject($opts));
         $client->setAdapter($curlAdapter);
         $api = new static($client, $session);
         self::setInstance($api);
